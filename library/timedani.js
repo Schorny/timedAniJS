fbea9829830bfb261af1de50c04d9e565b9599f3
Toni Schornboeck <toni.schornboeck@gmail.com>
2015-07-14 21:29:13 +0200

    added license info

--- library/timedani.js
+++ library/timedani.js
@@ -1,5 +1,8 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-(function(root, factory) {
+;(function(root, factory) {
     if(typeof define === 'function' && define.amd) {
         define(['jquery'], factory);
     } else {
