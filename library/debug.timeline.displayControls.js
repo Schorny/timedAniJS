c9a35a8e736840d93281752717f15a1086e7016b
nietonfir <nietonfir@gmail.com>
2015-09-27 15:48:14 +0200

    Reduce JS scope lookup

--- library/debug.timeline.displayControls.js
+++ library/debug.timeline.displayControls.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-;(function() {
+;(function($, document) {
     var origTimeline = TA.Timeline;
 
     TA.Timeline = function() {
@@ -60,4 +60,4 @@
 
         return origTimeline.apply(this, arguments);
     };
-})();
+})(jQuery, document);
